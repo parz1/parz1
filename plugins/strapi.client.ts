c382ec953d3f4569aa105d8a9223e4b8641e30f0
parz1 <parz1zhou@outlook.com>
2022-09-24 18:11:26 +0800

    delete all

--- plugins/strapi.client.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import type { Strapi4Error } from '@nuxtjs/strapi'
-import { defineNuxtPlugin } from '#app'
-export default defineNuxtPlugin((nuxt) => {
-  nuxt.hooks.hook('strapi:error' as any, (e: Strapi4Error) => {
-    nuxt.$toast.error({ title: e.error.name, description: e.error.message })
-  })
-})
